70e3c6f217ae2e5be2d34a78d0ce3773921526ff
AbdulazizRus <coderdim@gmail.com>
2025-08-28 16:14:36 +0500

    register setup success

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import "dotenv/config";
 import { Bot, GrammyError, HttpError } from "grammy";
+import mongoose from "mongoose";
+import { User } from "./models/User.js";
 
 const bot = new Bot(process.env.BOT_API_KEY!);
 
 // Ответ на команду /start
-bot.command("start", (ctx) =>
-  ctx.reply("Привет! Отправь мне любой текст, и я его повторю.")
-);
+bot.command("start", async (ctx) => {
+  if (!ctx.from) {
+    return ctx.reply("User info is not available");
+  }
+
+  const { id, username, first_name } = ctx.from;
+
+  try {
+    const existingUser = await User.findOne({ telegramId: id });
+
+    if (existingUser) {
+      return ctx.reply(`Welcome back, ${first_name}`);
+    }
+
+    const newUser = new User({
+      telegramId: id,
+      firstName: first_name,
+      username,
+    });
+
+    await newUser.save();
+
+    return ctx.reply(`You are registered, ${first_name}`);
+  } catch (error) {
+    console.error("Error in start command:", error);
+    ctx.reply("An error occurred while processing your request.");
+  }
+});
 
 // Ответ на любое сообщение
 bot.on("message:text", (ctx) => {
@@ -31,8 +58,9 @@ bot.catch((err) => {
 // Функция запуска бота
 async function startBot() {
   try {
+    await mongoose.connect(process.env.MONGODB_URI!);
     bot.start();
-    console.log("Bot started");
+    console.log("MOngo & Bot started");
   } catch (error) {
     console.error("Error in startBot:", error);
   }
---
+++ src/models/User.ts
@@ -0,0 +1,30 @@
+import { Document, model, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  telegramId: number;
+  firstName: string;
+  username: string;
+  createdAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    telegramId: {
+      type: Number,
+      required: [true, "Telegram ID is required"],
+      unique: true,
+    },
+    firstName: {
+      type: String,
+    },
+    username: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = model<IUser>("User", userSchema);
